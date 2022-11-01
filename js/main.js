27f658d72e05f0c9c96ee56b9523f41bb295bc35
Alexandr Otkidach <alexandr.otkidach@gmail.com>
2022-11-01 03:09:38 +0200

    add animate

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var header = $(".header");
+var scrollChange = 1;
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= scrollChange) {
+        header.addClass('scroll');
+    } else {
+        header.removeClass("scroll");
+    }
+});
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+});
+
+function myAnimate (el, animation){
+    $(`${el}`).mouseover(function(){
+        return $(this).addClass(`${animation}`);
+    })
+    $(`${el}`).mouseout(function(){
+        return $(this).removeClass(`${animation}`);
+    });
+
+}
+// animate('.btn','animated heartBeat infinite');
+myAnimate('.footer-logo','animated shake infinite slow');
+myAnimate('.contact-us,.basket','animated pulse infinite');
+
+
+$(document).ready(function() {
+    new WOW().init();
+});
