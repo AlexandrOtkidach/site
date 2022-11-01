00d3f0360d86a6c8bc7b8b1850b6d5ceb04dc1a5
Alexandr Otkidach <alexandr.otkidach@gmail.com>
2022-11-01 13:23:44 +0200

    add slider

--- js/main.js
+++ js/main.js
@@ -42,4 +42,32 @@ $(document).ready(function() {
     new WOW().init();
 });
 
-$('.scroll-top').click()
+window.addEventListener("load", function(event) {
+    document.querySelector('.scroll-top').click()
+});
+
+
+jQuery(document).ready(function(){
+    if(jQuery('#slider .logos.alt-arrow').length) {
+        jQuery('#slider .logos.alt-arrow').slick({
+          infinite: true,
+          slidesToScroll: 1,
+          slidesToShow: 3,
+          arrows: true,
+          prevArrow: '<div class="slick-prev"><svg width="15" height="40" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.25 2L2 13.25L13.25 24.5" stroke="#D9D9D9" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>',
+          nextArrow: '<div class="slick-next"><svg width="15" height="40" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.75 2L13 13.25L1.75 24.5" stroke="#D9D9D9" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>',
+          autoplay: true,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                rows: 1,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+    
+              }
+            }
+          ]
+        });
+    }
+});
